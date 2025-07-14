71de40df9df9c612dfeda240ae4af7980d3e8ac0
Theodor Petkov <theodor.petkov@upfitness.com>
2025-07-14 15:38:52 +0100

    Exported heat loss function from utils.

--- src/utils/heat-loss/calculate-heat-loss.ts
+++ src/utils/heat-loss/calculate-heat-loss.ts
@@ -2,6 +2,6 @@ export const calcHeatLoss = (
   floorArea: number,
   heatFactor: number,
   insulationFactor: number
-) => {
+): number => {
   return floorArea * heatFactor * insulationFactor;
 };
